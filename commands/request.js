0251de182f2d6e666ab8f7ef930dd71cc4da2cce
Xander <xanderhogan13@gmail.com>
2023-11-24 14:36:34 -0500

    Add files via upload

---
+++ commands/request.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('request-role')
+        .setDescription('Request a custom role.')
+        .addStringOption(option =>
+            option.setName('name')
+                .setDescription('What should your roles name be?')
+                .setRequired(true)),
+    async execute(interaction) {
+        const name = interaction.options.getString('name')
+        const commandUser = interaction.user
+        const commandUserId = commandUser?.id
+        
+        const testEmbed = new EmbedBuilder()
+        .setColor('#0099ff')
+        .setTitle('Someone has requested a role.')
+        .setDescription('<@${commandUserId}> has requested a role. \n The name: ${name}')
+        
+		console.log(name)
+            await interaction.reply({content: '<@829909201262084096>', embeds: [testEmbed]});
+        }
+    };
